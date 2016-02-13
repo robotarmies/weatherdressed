3ed986b481506fd561b9de35badb7f529a358156
James Bruner <james@robotarmies.com>
2016-02-13 12:22:40 -0500

    response test

--- echo/index.php
+++ echo/index.php
@@ -6,7 +6,7 @@ $json = $core->alexa();
 
 //TESTING THE REQUEST
 $file = fopen("test.txt","a");
-$post_json = $_REQUEST;
+$post_json = $_POST;
 $post = json_decode($post_json, true);
 foreach($post as $key=>$value) {
     $message = $key . ":" . $value . "\n";
