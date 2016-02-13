1d826ed12879e7717280d0ce8bb603fe3bbdd570
James Bruner <james@robotarmies.com>
2016-02-13 12:18:41 -0500

    testing the request

--- echo/index.php
+++ echo/index.php
@@ -1,8 +1,19 @@
 <?php
 require_once '../core.php';
 $core = new WeatherDressed();
-$x = $_REQUEST;
+//$x = $_REQUEST;
 $json = $core->alexa();
+
+//TESTING THE REQUEST
+$file = fopen("test.txt","a");
+$post_json = $_REQUEST;
+$post = json_decode($post_json, true);
+foreach($post as $key=>$value) {
+    $message = $key . ":" . $value . "\n";
+    echo fwrite($file,$message);
+}
+fclose($file);
+
 
 /* Output header */
 header('Content-type: application/json');
