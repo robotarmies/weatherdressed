5edb99fd449b134594ee151af7ab98b323e9ef46
James Bruner <james@robotarmies.com>
2016-02-12 20:52:10 -0500

    testing response

--- echo/index.php
+++ echo/index.php
@@ -7,5 +7,36 @@
  */
 
 $x = $_REQUEST;
+$resp_array = array(
+        "version" => "1.0",
+        "response" => array(
+            "outputSpeech" => array(
+                "type" => "Plaintext",
+                "text" => "How about your birthday suit."
+            )),
+        "card" => array(),
+        "reprompt" => null,
+        "shouldEndSession" => true,
+        "sessionAttributes" => null,
+        );
+$json = '{
+  "version": "1.0",
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "Turning up the Nest to an average temperature of 74 degrees."
+    },
+    "card": {
+      "type": "Simple",
+      "content": "Telling Nest to set to 74 degrees fahrenheit.",
+      "title": "Nest Control - Setting Nest Temp"
+    },
+    "reprompt": null,
+    "shouldEndSession": true
+  },
+  "sessionAttributes": null
+}';
 
-return "hooray";
+/* Output header */
+header('Content-type: application/json');
+echo json_encode($json);
