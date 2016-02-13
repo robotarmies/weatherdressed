cf2ca196f85b474f65f1166fb08e82dd51efda94
James Bruner <james@robotarmies.com>
2016-02-12 20:55:24 -0500

    initial live testing

--- echo/index.php
+++ echo/index.php
@@ -7,29 +7,18 @@
  */
 
 $x = $_REQUEST;
-$resp_array = array(
-        "version" => "1.0",
-        "response" => array(
-            "outputSpeech" => array(
-                "type" => "Plaintext",
-                "text" => "How about your birthday suit."
-            )),
-        "card" => array(),
-        "reprompt" => null,
-        "shouldEndSession" => true,
-        "sessionAttributes" => null,
-        );
+
 $json = '{
   "version": "1.0",
   "response": {
     "outputSpeech": {
       "type": "PlainText",
-      "text": "Turning up the Nest to an average temperature of 74 degrees."
+      "text": "Maybe you should wear your birthday suit."
     },
     "card": {
       "type": "Simple",
-      "content": "Telling Nest to set to 74 degrees fahrenheit.",
-      "title": "Nest Control - Setting Nest Temp"
+      "content": "Maybe You should wear your birthday suit.",
+      "title": "WeatherDressed"
     },
     "reprompt": null,
     "shouldEndSession": true
